91eec908a0347aabcd43cdd5e8db8c27cee7b4bc
Ayush Udayakumar Nair <ayushun2003@gmail.com>
2024-08-13 12:52:25 +0530

    Add NoteItem.jsx
    Added NoteItem component to display each note as a separate card.

--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react'
 import NoteContext from '../context/notes/NoteContext'
+import NoteItem from './NoteItem'
 
 const Notes = () => {
     const { notes, setNotes } = useContext(NoteContext)
     return (
-        <>
-        <h1 className='my-3'>Your notes</h1>
-        {notes.map((note) => {
-            return note.title
-        })}
-        </>
-  )
+        <div className='row'>
+            <h1 className='my-3'>Your notes</h1>
+            {notes.map((note) => {
+                return <NoteItem note={note} />
+            })}
+        </div>
+    )
 }
 
 export default Notes
