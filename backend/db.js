e5c23b2d9ac6c9bc417ee4017b451271047f6ef2
Ayush Udayakumar Nair <ayushun2003@gmail.com>
2024-09-19 07:44:28 +0530

    MongoDB Atlas configuration

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/JournalScript"
+require('dotenv').config();
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD
+const mongoURI = `mongodb+srv://ayushun2003:${MONGO_PASSWORD}@journalscript.nos0m.mongodb.net/?retryWrites=true&w=majority&appName=JournalScript`
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(() => console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
 }
