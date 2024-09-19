4421970569f12dc822d9d2f597595c6762b8f852
Ayush Udayakumar Nair <ayushun2003@gmail.com>
2024-09-19 09:30:38 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const mongoURI = "mongodb://localhost:27017/JournalScript"
+const MONGO_URI = process.env.MONGO_URI
+const mongoURI = MONGO_URI
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(() => console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
 }
