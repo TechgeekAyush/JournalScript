92c6d83cdcc105141116e6c89a17c8d557278fbf
Ayush Udayakumar Nair <ayushun2003@gmail.com>
2024-09-19 08:08:56 +0530

    Configurations for deployment

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -7,7 +7,7 @@ const Signup = (props) => {
   const onChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value })
   }
-  const host = "http://localhost:3000"
+  const host = "https://journalscript.onrender.com"
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch(`${host}/api/auth/createuser`, {
