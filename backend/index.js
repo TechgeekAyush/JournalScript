ab3e1bc896ce5915966792dd80d1054cadd2183b
Ayush Udayakumar Nair <ayushun2003@gmail.com>
2024-10-26 18:33:04 +0530

    Changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const connectToMongo = require('./db')
 const express = require('express')
 connectToMongo();
 const cors = require('cors')
-// const path = require('path')
+const path = require('path')
 const app = express()
 const port = 3000
 
@@ -12,12 +12,12 @@ app.use(express.json()) //to use req.body -> this middleware is required
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-//   })
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
 
 app.listen(port, () => {
   console.log(`JournalScript listening on port ${port}`)
